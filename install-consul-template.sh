664ec0df8145b27d5d3915bc1535a0a439c428f4
gomesuit <gomesuit@gmail.com>
2016-06-22 11:34:26 +0900

    Refactoring

--- install-consul-template.sh
+++ install-consul-template.sh
@@ -4,9 +4,3 @@ yum install -y wget unzip
 wget https://releases.hashicorp.com/consul-template/0.15.0/consul-template_0.15.0_linux_amd64.zip
 unzip -o consul-template_0.15.0_linux_amd64.zip
 cp -f consul-template /usr/local/bin/
-
-
-cat <<EOF > /root/hosts-node.ctmpl
-# consul nodes{{range nodes}}
-{{.Address}}    {{.Node}}{{end}}
-EOF
